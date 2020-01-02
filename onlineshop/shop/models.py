262588b988e1ceb35aeb724b632c4490b889f249
Igor <IggorsBot@gmail.com>
2020-01-02 15:48:05 +0300

    add orders app

--- onlineshop/shop/models.py
+++ onlineshop/shop/models.py
@@ -12,6 +12,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Highlight(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'highlight'
+        verbose_name_plural = 'highlights'
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
@@ -26,6 +40,17 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(Product):
+    author = models.CharField(max_length=100)
+    series = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    year_of_issue = models.PositiveSmallIntegerField()
+    cover = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
