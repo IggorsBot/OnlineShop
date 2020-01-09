2e5582ba25fb9fdfc36edc6e2c77d674647514b1
Igor <IggorsBot@gmail.com>
2020-01-09 21:45:36 +0300

    change class to className from react components

--- onlineshop/frontend/src/components/footer/ShippingMoneySupport.js
+++ onlineshop/frontend/src/components/footer/ShippingMoneySupport.js
@@ -4,33 +4,33 @@ import ReactDOM from 'react-dom'
 function ShippingMoneySupport(){
     return(
       <Fragment>
-      <div class="container-fluid p-0">
-        <div class="site-info">
-          <div class="row text-center py-3 bg-primary-color m-0">
-            <div class="col-md-4 col-sm-12 my-md-O my-4">
-              <div class="row justify-content-center-center text-light">
-                <i class="fas fa-rocket fa-4x px-4"></i>
-                <div class="py-2 font-roboto text-left">
-                  <h6 class="m-0">Free Shipping & Return</h6>
+      <div className="container-fluid p-0">
+        <div className="site-info">
+          <div className="row text-center py-3 bg-primary-color m-0">
+            <div className="col-md-4 col-sm-12 my-md-O my-4">
+              <div className="row justify-content-center-center text-light">
+                <i className="fas fa-rocket fa-4x px-4"></i>
+                <div className="py-2 font-roboto text-left">
+                  <h6 className="m-0">Free Shipping & Return</h6>
                   <small>Free Shipping on order over $49</small>
                 </div>
               </div>
             </div>
-            <div class="col-md-4 col-sm-12 my-md-0 my-4">
-              <div class="row justify-content-center-center text-light">
-                <i class="fas fa-hand-holding-usd fa-4x px-4"></i>
-                <div class="py-2 font-roboto text-left">
-                  <h6 class="m-0">Money Guarantee</h6>
+            <div className="col-md-4 col-sm-12 my-md-0 my-4">
+              <div className="row justify-content-center-center text-light">
+                <i className="fas fa-hand-holding-usd fa-4x px-4"></i>
+                <div className="py-2 font-roboto text-left">
+                  <h6 className="m-0">Money Guarantee</h6>
                   <small>30 days money back guarantee</small>
                 </div>
               </div>
             </div>
-            <div class="col-md-4 col-sm-12 my-md-0 my-4">
-              <div class="col-md-4 col-md-12 my-md-O my-4">
-                <div class="row justify-content-center-center text-light">
-                  <i class="fas fa-headphones-alt fa-4x px-4"></i>
-                  <div class="py-2 font-roboto text-left">
-                    <h6 class="m-0">Online Support</h6>
+            <div className="col-md-4 col-sm-12 my-md-0 my-4">
+              <div className="col-md-4 col-md-12 my-md-O my-4">
+                <div className="row justify-content-center-center text-light">
+                  <i className="fas fa-headphones-alt fa-4x px-4"></i>
+                  <div className="py-2 font-roboto text-left">
+                    <h6 className="m-0">Online Support</h6>
                     <small>We support online 24 hours a day</small>
                   </div>
                 </div>
